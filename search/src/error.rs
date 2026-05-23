0b1b57d44c5dd0821c95cb8a58d35899ac346874
Louis <836250617@qq.com>
2026-05-23 14:23:36 +0800

    remove unused error module

--- search/src/error.rs
+++
@@ -1,2 +0,0 @@
-// Reserved for typed errors if you want richer handling later.
-// Currently SSE handlers stream "error" events instead of returning HTTP errors.
--- search/src/main.rs
+++ search/src/main.rs
@@ -1,4 +1,3 @@
-mod error;
 mod handlers;
 mod payload;
 mod server;
