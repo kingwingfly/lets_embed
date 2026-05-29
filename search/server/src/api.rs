fb98e40f741ddd06fbc7e048ef3f1c5b884603f0
kingwingfly <836250617@qq.com>
2026-05-29 23:10:58 +0800

    fix server function name not sanitized bug

--- search/server/src/api.rs
+++ search/server/src/api.rs
@@ -91,58 +91,60 @@ pub async fn search_sse(
                 }
             }
             Mode::Similar => {
-                match q.parse::<i64>() {
-                    Ok(id) => {
-                        match engine.image_details(id).await {
-                            Ok(details) => {
-                                let post_images = match details.post.map(|p| p.id) {
-                                    Some(post_id) => engine.list_post_images(post_id).await.ok(),
-                                    None => None
-                                };
-                                let post_image_ids = post_images.map(|post_images| post_images.into_iter().map(|i| i.id).collect::<HashSet<_>>());
-                                let mut path = (*path).to_owned();
-                                path.push(sanitize(details.image.name));
-                                path.add_extension("webp");
-                                match File::open(path) {
-                                    Ok(file) => {
-                                        let reader = BufReader::new(file);
-                                        match engine.search_dinov3([reader], limit, offset).await {
-                                            Ok(mut images) => {
-                                                let mut count = 0;
+                let prepared = async {
+                    let id = q.parse::<i64>().map_err(|e| e.to_string())?;
+                    let details = engine.image_details(id).await.map_err(|e| e.to_string())?;
 
-                                                while let Some(img) = images.next().await {
-                                                    count += 1;
-                                                    if post_image_ids.as_ref().is_some_and(|ids| ids.contains(&img.id)) {
-                                                        continue;
-                                                    }
-                                                    let item = ImageItem {
-                                                        id: img.id,
-                                                        name: sanitize(img.name),
-                                                        width: img.width as u32,
-                                                        height: img.height as u32,
-                                                    };
-                                                    yield Ok(Event::default()
-                                                        .event("image")
-                                                        .json_data(item)
-                                                        .unwrap());
-                                                }
+                    let post_image_ids = match details.post.as_ref().map(|p| p.id) {
+                        Some(post_id) => engine
+                            .list_post_images(post_id)
+                            .await
+                            .ok()
+                            .map(|imgs| imgs.into_iter().map(|i| i.id).collect::<HashSet<_>>()),
+                        None => None,
+                    };
 
-                                                let has_more = (count as i64) >= limit;
-                                                yield Ok(Event::default()
-                                                    .event("done")
-                                                    .json_data(DoneEvent { has_more })
-                                                    .unwrap());
-                                            }
-                                            Err(e) => yield Ok(send_err(e.to_string())),
-                                        }
-                                    },
-                                    Err(e) => yield Ok(send_err(e.to_string())),
-                                }
+                    let mut path = (*path).to_owned();
+                    path.push(sanitize(details.image.name));
+                    path.add_extension("webp");
+
+                    let file = File::open(&path).map_err(|e| e.to_string())?;
+                    let reader = BufReader::new(file);
+                    let images = engine
+                        .search_dinov3([reader], limit, offset)
+                        .await
+                        .map_err(|e| e.to_string())?;
+
+                    Ok::<_, String>((images, post_image_ids))
+                }
+                .await;
+
+                match prepared {
+                    Err(e) => yield Ok(send_err(e)),
+                    Ok((mut images, post_image_ids)) => {
+                        let mut count = 0i64;
+                        while let Some(img) = images.next().await {
+                            count += 1;
+                            if post_image_ids.as_ref().is_some_and(|ids| ids.contains(&img.id)) {
+                                continue;
                             }
-                            Err(e) => yield Ok(send_err(e.to_string())),
+                            let item = ImageItem {
+                                id: img.id,
+                                name: sanitize(img.name),
+                                width: img.width as u32,
+                                height: img.height as u32,
+                            };
+                            yield Ok(Event::default()
+                                .event("image")
+                                .json_data(item)
+                                .unwrap());
                         }
+
+                        yield Ok(Event::default()
+                            .event("done")
+                            .json_data(DoneEvent { has_more: count >= limit })
+                            .unwrap());
                     }
-                    Err(e) => yield Ok(send_err(e.to_string())),
                 }
             }
             _ => {}
